bd4ec72133904b352f34a9b56b4dae5e47908152
Marc Schmidt <marc.schmidt@serviceware.de>
2023-06-21 11:25:33 +0200

    feat(core): add ability to ignore parts of the Open API specification using 'x-ignore'

--- packages/core/src/lib/collect/helpers.ts
+++ packages/core/src/lib/collect/helpers.ts
@@ -1,7 +1,7 @@
 import { OpenApiCollectorData } from './types';
 import { isOpenApiObjectProperty } from '../internal-utils';
 
-export function collect<T>(
+export function collect<T extends Record<`x-${string}`, unknown>>(
   data: OpenApiCollectorData,
   obj: T | T[] | undefined,
   func: (data: OpenApiCollectorData, obj: NonNullable<T>) => void
@@ -10,14 +10,16 @@ export function collect<T>(
   if (Array.isArray(obj)) {
     for (const o of obj) {
       if (!o) continue;
+      if (o['x-ignore']) continue;
       func(data, o);
     }
   } else {
+    if (obj['x-ignore']) return;
     func(data, obj);
   }
 }
 
-export function collectRecord<T>(
+export function collectRecord<T extends Record<`x-${string}`, unknown>>(
   data: OpenApiCollectorData,
   obj: Record<string, T> | undefined,
   func: (data: OpenApiCollectorData, obj: NonNullable<T>, key: string) => void
@@ -27,6 +29,7 @@ export function collectRecord<T>(
     if (!isOpenApiObjectProperty(key)) continue;
     const value = obj[key];
     if (!value) continue;
+    if (value['x-ignore']) continue;
     func(data, value, key);
   }
 }
