05c161ceee3c7cc9933bc8e00d38737ee2ea69e8
Marc Schmidt <code@masch212.de>
2025-02-06 16:08:28 +0100

    feat(kotlin): add more jakarta validation annotations to models (#41)

--- packages/kotlin/src/ast/references/jakarta.ts
+++ packages/kotlin/src/ast/references/jakarta.ts
@@ -8,3 +8,7 @@ export const valid: KtReferenceFactory = ktReference.factory('Valid', 'jakarta.v
 
 // jakarta.validation.constraints
 export const pattern: KtReferenceFactory = ktReference.factory('Pattern', 'jakarta.validation.constraints');
+export const min: KtReferenceFactory = ktReference.factory('Min', 'jakarta.validation.constraints');
+export const max: KtReferenceFactory = ktReference.factory('Max', 'jakarta.validation.constraints');
+export const notEmpty: KtReferenceFactory = ktReference.factory('NotEmpty', 'jakarta.validation.constraints');
+export const size: KtReferenceFactory = ktReference.factory('Size', 'jakarta.validation.constraints');
