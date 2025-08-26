67a1680918ac9060576e09e14d5046c74693ea36
Marc Schmidt <marc.schmidt@serviceware.de>
2025-08-26 10:24:34 +0200

    fix(kotlin): disable FAIL_ON_UNKNOWN_PROPERTIES feature

--- packages/kotlin/assets/client/okhttp3/Serializer.kt
+++ packages/kotlin/assets/client/okhttp3/Serializer.kt
@@ -1,6 +1,7 @@
 package @PACKAGE_NAME@
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -11,4 +12,5 @@ object Serializer {
         .findAndRegisterModules()
         .setSerializationInclusion(JsonInclude.Include.@JSON_INCLUDE@)
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
